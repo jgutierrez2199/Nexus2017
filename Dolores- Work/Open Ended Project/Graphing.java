3068ae493e8c81eacf4b4727aa339eabe24c442e
VictoriaMen2a <toria.mendoza@live.com>
2017-07-17 10:25:27 -0600

    Add files via upload

--- Dolores- Work/Open Ended Project/Graphing.java
+++ Dolores- Work/Open Ended Project/Graphing.java
@@ -53,7 +53,19 @@ public class Graphing{
         System.out.println("Please enter the coeffecient for the " + i + " degree term.");
         coeff[i] = scnr.nextDouble();
       }
+        
+    }
+    
+    public static void exponential(){
+      System.out.println("What is the z and y of the exponential?");
+      Scanner scnr = new Scanner(System.in);
+      z y = scnr.nextInt();
+      double [] coeff = new double [ z y + 1];
 
+      for(z y i = 0; i < coeff.length; i++){
+        System.out.println("Please enter the value for z y");
+        coeff[i] = scnr.nextDouble();
+      }
     }
 
     //Use a switch statement to determine which specific function
@@ -105,6 +117,7 @@ public class Graphing{
 
         double a = 1.0;
         double b = 1.0;
+        double c = 1.0;
 
         switch(scnr.nextLine().charAt(0)){
           case 'a': case 'A':
@@ -114,7 +127,7 @@ public class Graphing{
             a = scnr.nextDouble();
             System.out.println("Please enter a value for b");
             b = scnr.nextDouble();
-            //graphlog(a,x);
+            //graphlog(a,b);
             break;
           case 'x': case 'X':
             System.out.println("What are the coeff. for a and x in the form");
@@ -123,25 +136,47 @@ public class Graphing{
             a = scnr.nextDouble();
             System.out.println("Please enter a value for b");
             b = scnr.nextDouble();
-            //graphln(a,x);
+            //graphln(a,b);
     }
         public static void rational(){
         Scanner scnr = new Scanner(System.in);
+            
+        double a = 1.0;
+        double b = 1.0;
+
+            System.out.println("What are the coeff. for a and b in the form");
+            System.out.println("\'a/bx^c \'");
+            System.out.println("Please enter a value for a");
+            a = scnr.nextDouble();
+            System.out.println("Please enter a value for b");
+            b = scnr.nextDouble();
+            System.out.println("Please enter a value for c");
+            c = scnr.nextDouble();
+            //graphlog(a,b);
+            break;
+            
+      public static void exponential(){
+        Scanner scnr = new Scanner(System.in);
+        System.out.println("What specific function would you like to graph?");
+        System.out.println("a) e^x(x)");
+        System.out.println("b) zyx(x)");
 
         double a = 1.0;
         double b = 1.0;
         double c = 1.0;
 
-        System.out.println("What are the coeff. for a and b in the form");
-        System.out.println("\'a/bx^c \'");
-        System.out.println("Please enter a value for a");
-        a = scnr.nextDouble();
-        System.out.println("Please enter a value for b");
-        b = scnr.nextDouble();
-        System.out.println("Please enter a value for c");
-        c = scnr.nextDouble();
+        switch(scnr.nextLine().charAt(0)){
+          case 'a': case 'A':
+            System.out.println("What are the coeff. for a and x in the form");
+            System.out.println("\'a e^x(b x)\'");
+            System.out.println("Please enter a value for a");
+            a = scnr.nextDouble();
+            System.out.println("Please enter a value for b");
+            b = scnr.nextDouble();
             //graphlog(a,b);
-
+            break;
+            
+    }
     //Take in the parameters needed
 
     //Generate Points
